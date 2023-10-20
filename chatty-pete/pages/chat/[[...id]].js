dad182e399742722f1ad59c68f7a9177b92976f7
Ignacio Valenzuela <ignaciovalen1@gmail.com>
2023-10-20 15:30:02 -0700

    Implement Message component

--- chatty-pete/pages/chat/[[...id]].js
+++ chatty-pete/pages/chat/[[...id]].js
@@ -2,16 +2,28 @@ import { ChatSidebar } from "components/ChatSidebar";
 import Head from "next/head";
 import { useState } from "react";
 import { streamReader } from "openai-edge-stream";
+import { v4 as uuid } from "uuid";
+import { Message } from "components/Message";
 
 export default function ChatPage() {
   const [messageText, setMessageText] = useState("");
   const [incomingMessage, setIncomingMessage] = useState("");
+  const [newChatMessages, setNewChatMessages] = useState([]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    console.log("messageText");
-    console.log(messageText);
+    setNewChatMessages((prev) => {
+      const newChatMessages = [
+        ...prev,
+        {
+          _id: uuid(),
+          role: "user",
+          content: messageText,
+        },
+      ];
+      return newChatMessages;
+    });
 
     const response = await fetch(`/api/chat/sendMessage`, {
       method: "POST",
@@ -25,17 +37,11 @@ export default function ChatPage() {
 
     const data = response.body;
 
-    console.log("data");
-    console.log(data);
-
     if (!data) {
-      console.log("NO DATA!!!!");
       return;
     }
 
     const reader = data.getReader();
-    console.log("reader!!!");
-    console.log(reader);
 
     await streamReader(reader, (message) => {
       setIncomingMessage((s) => `${s}${message.content}`);
@@ -49,7 +55,20 @@ export default function ChatPage() {
       <div className="grid h-screen grid-cols-[260px_1fr]">
         <ChatSidebar />
         <div className="flex flex-col bg-gray-700">
-          <div className="flex-1 text-white">{incomingMessage}</div>
+          <div className="flex-1 text-white">
+            {newChatMessages.map((message) => (
+              <Message
+                key={message._id}
+                role={message.role}
+                content={message.content}
+              />
+            ))}
+            {incomingMessage ? (
+              <Message role="assistant" content={incomingMessage} />
+            ) : (
+              ""
+            )}
+          </div>
           <footer className="bg-gray-800 p-10">
             <form onSubmit={handleSubmit}>
               <fieldset className="flex gap-2">
