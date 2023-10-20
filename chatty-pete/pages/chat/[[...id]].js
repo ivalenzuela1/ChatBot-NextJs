ecc1d41e132e4ce097de548020a3fb0e76297b89
Ignacio Valenzuela <ignaciovalen1@gmail.com>
2023-10-20 12:27:41 -0700

    create chat page

---
+++ chatty-pete/pages/chat/[[...id]].js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Welcome to chat page</title>
+      </Head>
+      <h1>Welcome to chat page</h1>
+    </div>
+  );
+}
