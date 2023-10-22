4806131210697245df92c987933a13c99f5b8c83
Ignacio Valenzuela <ignaciovalen1@gmail.com>
2023-10-22 13:41:10 -0700

    Add local state to save chats and render in Sidebar

--- chatty-pete/components/ChatSidebar/ChatSidebar.js
+++ chatty-pete/components/ChatSidebar/ChatSidebar.js
@@ -1,22 +1,31 @@
 import Link from "next/link";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export const ChatSidebar = () => {
+  const [chatList, setChatList] = useState([]);
   useEffect(() => {
     const loadChatList = async () => {
       const response = await fetch(`/api/chat/getChatList`, {
         method: "POST",
       });
-      const json = await response.json;
-
-      console.log("json");
-      console.log(json);
+      const json = await response.json();
+      setChatList(json?.chats || []);
     };
 
     loadChatList();
   }, []);
   return (
-    <div className="bg-gray-900 text-white">
+    <div className="flex flex-col overflow-hidden bg-gray-900 text-white">
+      <Link href="/chat">New Chat</Link>
+      <div className="flex-1 overflow-auto bg-gray-950">
+        {chatList.map((chat) => {
+          return (
+            <Link href={`/chat/${chat._id}`} key={chat._id}>
+              {chat.title}
+            </Link>
+          );
+        })}
+      </div>
       <Link href="/api/auth/logout">Log out</Link>
     </div>
   );
